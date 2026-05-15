451fa895b8b6793c3a3ca3be7d5a8f35763c293b
Neto Trindade <127351330+NETONoHands@users.noreply.github.com>
2026-05-14 21:32:04 -0300

    export type change

--- api/src/types.ts
+++ api/src/types.ts
@@ -1,14 +1,14 @@
-type Pokemon = {
+export type Pokemon = {
   id: number;
   name: string;
   types: string[];
 };
 
-type PokemonResponse = {
+export type PokemonResponse = {
   id: number;
   name: string;
   types: string[];
   weakness: string[];
 }
 
-export type { Pokemon, PokemonResponse };
+
