12bef34ca934e947d177115c968491e2f75a167d
Neto Trindade <127351330+NETONoHands@users.noreply.github.com>
2026-05-14 21:01:14 -0300

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,16 +2,40 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+//export default defineConfig({
+//  plugins: [react()],
+//  server: {
+//    port: 5173,
+//    proxy: {
+//      '/api': {
+//        target: 'http://localhost:8080', // URL do servidor backend
+//        changeOrigin: true,
+//        rewrite: (path) => path.replace(/^\/api/, '')
+//      }
+//    }
+//  }
+//})
+
+
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // URL do servidor backend
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+
+  // GitHub Pages (project site): https://<USER>.github.io/<REPO>/
+  base: '/exercicio-pokemon-types/',
+
+  // Tudo dentro de server.* é voltado ao DEV (vite dev server)
+  ...(command === 'serve'
+    ? {
+        server: {
+          port: 5173,
+          proxy: {
+            '/api': {
+              target: 'http://localhost:8080',
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+          },
+        },
       }
-    }
-  }
-})
+    : {}),
+}))
